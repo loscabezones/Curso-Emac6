4516a6910ccaacf0279382789380d1213bc8e1bd
USERSAD\loscabezones <david_gaia2@hotmail.com>
2018-11-07 15:46:13 +0100

    ejercicio 1 de introduccion

---
+++ ejericico1/app.js
@@ -0,0 +1,10 @@
+"use strict";
+// Un archivo simple de TypeScript
+function saludar(nombre) {
+    console.log(nombre.toUpperCase());
+}
+;
+var wolverin = {
+    nombre: "Logan"
+};
+saludar(wolverin.nombre);
---
+++ ejericico1/app.ts
@@ -0,0 +1,11 @@
+// Un archivo simple de TypeScript
+
+function saludar(nombre:string){
+    console.log(nombre.toUpperCase())
+};
+
+var wolverin = {
+    nombre: "Logan"
+};
+
+saludar(wolverin.nombre);
